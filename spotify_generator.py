e10a4f13ec44ab98e62a880d5db00b22c2e4eb5f
shivanggupta1509 <87229803+shivanggupta1509@users.noreply.github.com>
2022-08-30 03:01:27 +0530

    Update spotify_generator.py

--- spotify_generator.py
+++ spotify_generator.py
@@ -6,7 +6,8 @@ def getRandomString(length): #Letters and numbers
 
 def getRandomText(length): #Chars only
     return "".join(random.choice(string.ascii_lowercase) for i in range(length))
-def dripplay: ('dripplay')
+def dripplay
+Dripplay=('dripplay')
 def generate():
     nick = input('[username]-')
     passw = ('shivanggupta')
